f8b0c576d2abea54690faeec92bead25f702974f
David Leiva <david.leiva04@gmail.com>
2023-09-02 01:21:18 -0600

    updates in index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,21 +1,26 @@
-import preline from 'preline/plugin.js';
-
+import preline from "preline/plugin.js";
 
 export default {
-  content: [
-    './public/**/*.astro',
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-    'node_modules/preline/dist/*.js',
-  ],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      fontFamily: {
-        jost: [
-          'Jost'  
-        ],
-      },
+    content: [
+        "./public/**/*.astro",
+        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+        "node_modules/preline/dist/*.js",
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            fontFamily: {
+                jost: ["Jost"],
+            },
+        },
     },
-  },
-  plugins: [preline],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
+    plugins: [preline],
 };
